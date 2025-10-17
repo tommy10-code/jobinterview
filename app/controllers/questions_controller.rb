3018c614407269cd7a8d8e256653a4246b1837e8
tommy10-code <mtywp058@gmail.com>
2025-10-17 12:57:12 +0900

    コントローラー作成まで

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+  def index
+  end
+
+  #今回showは使わない
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
